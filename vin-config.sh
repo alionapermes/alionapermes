b08b134f874e3bfdc1345d38ed100f8c7a45e403
Timofey Moskvin <alionapermes@gmail.com>
2022-01-25 21:33:21 +0700

    add clang-format install commands

--- vin-config.sh
+++ vin-config.sh
@@ -4,6 +4,8 @@ INSTALL="pacman -S"
 NVIM_PATH="~/.config/nvim"
 
 sudo ${INSTALL} nvim ccls python3 pip
+#pip install clang-format
+#pip install neovim
 
 if [[ ! -d ${NVIM_PATH} ]]; then
     mkdir -p ${NVIM_PATH}
